391f235ee6290bc6cf909213676963019beee4e9
csyxwei <yuxiang.wei.cs@gmail.com>
2021-11-03 22:31:53 +0800

    update the replicate input

--- simplegan_experiments/predict.py
+++ simplegan_experiments/predict.py
@@ -17,47 +17,36 @@ class Predictor(cog.Predictor):
         self.net.load_state_dict(state_dict)
         self.net.eval()
 
-
-    @cog.input("param",
-               type=str,
-               default='{"eyeglasses":0, '
-                       '"gender":1, '
-                       '"fringe orientation":0, '
-                       '"hair color":1.5, '
-                       '"fringe length":1, '
-                       '"horizontal pose":0, '
-                       '"vertical pose":0, '
-                       '"light":1}',
-
-               help="Paramters for generation. \n"
-                    "For eyeglasses, better in [-5, 2], minus is add eyeglasses. \n"
-                    "For gender, better in [-2, 2], minus is change to male. \n"
-                    "For fringe orientation, better in [-3, 3], minus is toward to the left. \n"
-                    "For hair color, better in [-3, 3], minus is to be blond. \n"
-                    "For fringe length, better in [-3, 3], minus is to be short. \n"
-                    "For horizontal pose, better in [-3, 3], minus is toward to the right. \n"
-                    "For vertical pose, better in [-5, 2], minus is toward to the up. \n"
-                    "For light, better in [-2, 2], minus is to be light.")
-
-    @cog.input("seed", type=float, default=0, help="Factor to control the sampled latent input")
-    def predict(self, param, seed):
+    @cog.input("eyeglasses", type=float, default=0, min=-5, max=2,
+               help="Factor to control the eyeglasses, better in [-5, 2], minus is adding eyeglasses")
+    @cog.input("gender", type=float, default=1, min=-2, max=2,
+               help="Factor to control the gender, better in [-2, 2], minus is changng to male")
+    @cog.input("fringe_orientation", type=float, default=0, min=-3, max=3,
+               help="Factor to control the fringe orientation, better in [-3, 3], minus is moving to the left")
+    @cog.input("hair_color", type=float, default=1.5, min=-3, max=3,
+               help="Factor to control the hair color, better in [-3, 3], minus is to be blond")
+    @cog.input("fringe_length", type=float, default=1, min=-3, max=3,
+               help="Factor to control the fringe length, better in [-3, 3], minus is to be short")
+    @cog.input("pose_horizontal", type=float, default=0, min=-3, max=3,
+               help="Factor to control the horizontal pose, better in [-3, 3], minus is moving to the rignt")
+    @cog.input("pose_vertical", type=float, default=0, min=-5, max=2,
+               help="Factor to control the vertical pose, better in [-5, 2], minus is moving to the up")
+    @cog.input("lightness", type=float, default=1, min=-2, max=2,
+               help="Factor to control the lightness, better in [-2, 2], minus is to be light")
+    @cog.input("seed", type=int, default=0, help="Factor to control the sampled latent input")
+    def predict(self, eyeglasses, gender, fringe_orientation, hair_color, fringe_length, pose_horizontal, pose_vertical, lightness, seed):
         """Run a single prediction on the model"""
         torch.manual_seed(seed)
 
-        try:
-            param_dict = json.loads(param)
-        except ValueError:
-            return "param dict is invalid!"
-
         z = torch.randn(1, 30)
-        z[:, 0] = float(param_dict['eyeglasses'])
-        z[:, 2] = float(param_dict['gender'])
-        z[:, 3] = float(param_dict['fringe orientation'])
-        z[:, 10] = float(param_dict['hair color'])
-        z[:, 23] = float(param_dict['fringe length'])
-        z[:, 24] = float(param_dict['horizontal pose'])
-        z[:, 26] = float(param_dict['vertical pose'])
-        z[:, 27] = float(param_dict['light'])
+        z[:, 0] = eyeglasses
+        z[:, 2] = gender
+        z[:, 3] = fringe_orientation
+        z[:, 10] = hair_color
+        z[:, 23] = fringe_length
+        z[:, 24] = pose_horizontal
+        z[:, 26] = pose_vertical
+        z[:, 27] = lightness
         img = self.net(z)
         img = img[0].cpu().detach().numpy()
         img = (img.transpose((1, 2, 0)) + 1) * 127.5
